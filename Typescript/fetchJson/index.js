6e91a3e4fd6a5716f620b4213ec57cb129b66f6a
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2026-05-05 02:39:45 +0530

    Getting started with typescripts interface

---
+++ Typescript/fetchJson/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+axios.get(url).then(response => {
+    // Response.data has properties of :
+    // id
+    // title
+    // completed
+    // We need interface to tell about the api load
+    const todo = response.data;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    logTodo(id, title, completed);
+});
+const logTodo = (id, title, completed) => {
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of : ${title}
+        Is it finished? ${completed}
+    `);
+};
